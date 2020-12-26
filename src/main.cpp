28786bb9ae826d421eda967de5ea9a740bd6a09f
Phil <p.m.birch@sussex.ac.uk>
2020-12-26 19:30:03 +0000

    tidy up

--- src/main.cpp
+++ src/main.cpp
@@ -13,52 +13,37 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {6, 5, 4, 3}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {8, 2, 7}; //connect to the column pinouts of the keypad
 byte ledPins[]={10,11,12,13,A0,A1,A2}; //pins for LEDS
-
+const long interval = 100; //mS delay for flashing lights
 
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 byte ledState[]={0,0,0,0,0,0,0};
-unsigned long loopCount = 0;
-unsigned long timer_t = 0;
 byte zoom_counter=0;
 
 void setup(){
-	Serial.begin(9600);
+	Serial.begin(9600);  // only used for debugging, ie printing
 	
 	// Try playing with different debounceTime settings to see how it affects
 	// the number of times per second your loop will run. The library prevents
 	// setting it to anything below 1 millisecond.
 	kpd.setDebounceTime(10);	// setDebounceTime(mS)
-  for(int i=0;i<NUM_LEDS;i++)
+  for(int i=0;i<NUM_LEDS;i++)  //set LED pins to output
   {
      pinMode(ledPins[i], OUTPUT);
   }
 }
 
-enum run_mode{single,party,zoom,off,allon, flash};
-
+//define the possible LED modes
+enum run_mode{single,party,zoom,off,allon, flash}; 
 enum run_mode mode=single;
-int val=0;
+int val=0;  //last numeric key pressed
 unsigned long previousMillis = 0;        // will store last time LED was updated
 bool timeout = 0;
-// constants won't change:
-const long interval = 100; 
+
+
 
 void loop(){
 	char key = kpd.getKey();
-	
-	// Report the number of times through the loop in 1 second. This will give
-	// you a relative idea of just how much the debounceTime has changed the
-	// speed of your code. If you set a high debounceTime your loopCount will
-	// look good but your keypresses will start to feel sluggish.
-	if ((millis() - timer_t) > 1000) {
-		Serial.print("Your loop code ran ");
-		Serial.print(loopCount);
-		Serial.println(" times over the last second");
-		loopCount = 0;
-		timer_t = millis();
-	}
-	loopCount++;
-	if(key)
+	if(key)  // if a key is pressed process it.
   {
       
     if(key=='#')
@@ -74,7 +59,7 @@ void loop(){
     }
     else
     {
-      val = atoi(&key);
+      val = atoi(&key); //if it wasn't # or * it must be a number
       if (val == 0)
       {
         mode = off;
@@ -91,12 +76,12 @@ void loop(){
       {
         mode = single;
         ledState[val-1]=~ledState[val-1];
-        Serial.println("***************************************************************************");
       }
     }
   }
   unsigned long currentMillis = millis();
   timeout = currentMillis - previousMillis >= interval;
+  //now process, according to the current mode
   if(timeout)
     previousMillis=currentMillis;
 
